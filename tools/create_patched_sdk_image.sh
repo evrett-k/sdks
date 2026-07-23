21d56c7a3810d341bb0f5604eed9cf963db84858
evrett.k <everett.kamulda@outlook.com>
2026-07-22 19:37:10 -1000

    Update create_patched_sdk_image.sh

--- tools/create_patched_sdk_image.sh
+++ tools/create_patched_sdk_image.sh
@@ -36,6 +36,8 @@ SYMBOLS_DIR="$(mktemp -d -t symbols)"
 
 make dsc_extractor_client
 
+chmod +x ./dsc_extractor_client
 ./dsc_extractor_client "${PLATFORM_PATH}/usr/lib/dsc_extractor.bundle" "${DYLD_CACHE_PATH}" "${SYMBOLS_DIR}"
 
+chmod +x ./create_patched_sdk.sh
 ./create_patched_sdk.sh "${SYMBOLS_DIR}" "${SDK_OUTPUT_PATH}" "${BASE_SDK_PATH}" "${TBD_TOOL_PATH}"
